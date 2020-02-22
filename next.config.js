4ffc596072d1f78c36ffb04f64292b226ae7a816
Jang Haemin <io@jhaemin.com>
2020-02-22 15:15:26 -0500

    chore: path alias

--- next.config.js
+++ next.config.js
@@ -1,2 +1,9 @@
+const path = require('path')
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({})
+
+module.exports = withSass({
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    return config
+  },
+})
