9be447bb8cd569f444f30e0ca0b92caef3b73542
Jang Haemin <io@jhaemin.com>
2020-02-22 15:17:39 -0500

    feat: create home content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const Button = styled.button`
-  display: 'flex';
-`
+import BaseLayout from '@/layouts/BaseLayout'
+import '@/assets/styles/pages/home.scss'
 
 const Home: React.FC = () => {
-  return <Button />
+  return (
+    <BaseLayout>
+      <div id="home">
+        <h1 className="header">어디로</h1>
+        <p>중앙대학교 유틸리티 서비스</p>
+      </div>
+    </BaseLayout>
+  )
 }
 
 export default Home
