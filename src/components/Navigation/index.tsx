0c14d18be9d7d836da528b9badebde42c05823aa
Jang Haemin <io@jhaemin.com>
2020-02-22 22:33:23 -0500

    feat: update Navigation

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -21,14 +21,16 @@ const NavItem: React.FC<NavItemProps> = ({ to, title }) => {
 const Navigation: React.FC = () => {
   return (
     <nav id="eodiro-navigation">
-      <EodiroLogo className="eodiro-logo" />
-      <ul className="en-menus-container">
-        <NavItem title="빈 강의실" to="/" />
-        <NavItem title="강의 검색" to="/" />
-        <NavItem title="학식 검색" to="/" />
-        <NavItem title="빼빼로 광장" to="/" />
-        <NavItem title="더 보기" to="/" />
-      </ul>
+      <div className="en-wrapper">
+        <EodiroLogo className="eodiro-logo" />
+        <ul className="en-menus-container">
+          <NavItem title="빈 강의실" to="/" />
+          <NavItem title="강의 검색" to="/" />
+          <NavItem title="학식 메뉴" to="/" />
+          <NavItem title="빼빼로 광장" to="/" />
+          <NavItem title="더 보기" to="/" />
+        </ul>
+      </div>
     </nav>
   )
 }
