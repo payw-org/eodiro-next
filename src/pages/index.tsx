9904d81d095c62e138fa94000c42e7215e6f35d2
Jang Haemin <io@jhaemin.com>
2020-02-22 22:33:44 -0500

    feat: update home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,43 @@
 import React from 'react'
 import BaseLayout from '@/layouts/BaseLayout'
 import '@/assets/styles/pages/home.scss'
+import Grid from '@/layouts/Grid'
+import { ColorIcon } from '@/components/icons/types/icon'
+import CafeteriaIconColor from '@/components/icons/CafeteriaIconColor'
+import VacantIconColor from '@/components/icons/VacantIconColor'
+import LecturesIconColor from '@/components/icons/LecturesIconColor'
+import SquareIconColor from '@/components/icons/SquareIconColor'
+import OpensourceIconColor from '@/components/icons/OpensourceIconColor'
+
+type HomeFeatureBoxProps = {
+  title: string
+  Icon: ColorIcon
+}
+
+const HomeFeatureBox: React.FC<HomeFeatureBoxProps> = ({ title, Icon }) => {
+  return (
+    <button className="feature-box">
+      <Icon className="icon" />
+      <h2 className="feature-name">{title}</h2>
+    </button>
+  )
+}
 
 const Home: React.FC = () => {
   return (
     <BaseLayout>
-      <div id="home">
+      <div id="eodiro-home">
         <h1 className="header">어디로</h1>
-        <p>중앙대학교 유틸리티 서비스</p>
+        <p className="manifesto">중앙대 학생들만을 위한 특별한 서비스</p>
+        <div className="features">
+          <Grid>
+            <HomeFeatureBox title="빈 강의실" Icon={VacantIconColor} />
+            <HomeFeatureBox title="강의 검색" Icon={LecturesIconColor} />
+            <HomeFeatureBox title="학식 메뉴" Icon={CafeteriaIconColor} />
+            <HomeFeatureBox title="빼빼로 광장" Icon={SquareIconColor} />
+            <HomeFeatureBox title="오픈 소스" Icon={OpensourceIconColor} />
+          </Grid>
+        </div>
       </div>
     </BaseLayout>
   )
