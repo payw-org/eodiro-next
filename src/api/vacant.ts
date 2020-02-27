cea022c9cb3057d6a36d5881a8286622137aecfd
Jang Haemin <io@jhaemin.com>
2020-02-27 12:45:05 -0600

    feat(api): vacant classrooms using useFetch

--- src/api/vacant.ts
+++ src/api/vacant.ts
@@ -1,5 +1,6 @@
 import getApiHost from '@/modules/get-api-host'
 import { Campus, Year, Semester } from '@/types'
+import useFetch from '@/modules/use-fetch'
 
 export default class VacantApi {
   static async buildings({
@@ -11,16 +12,38 @@ export default class VacantApi {
     semester: Semester
     campus: Campus
   }) {
-    try {
-      const res = await fetch(
-        getApiHost() + `/${year}/${semester}/${campus}/vacant/buildings`,
-        {
-          method: 'get',
-        }
-      )
-      return res.json()
-    } catch (err) {
-      return err
-    }
+    const [err, data] = await useFetch(
+      getApiHost() +
+        `/${year}/${semester}/${encodeURIComponent(campus)}/vacant/buildings`,
+      {
+        method: 'get',
+      }
+    )
+
+    return err ? null : data
+  }
+
+  static async classrooms({
+    year,
+    semester,
+    campus,
+    building,
+  }: {
+    year: Year
+    semester: Semester
+    campus: Campus
+    building: string
+  }) {
+    const [err, data] = await useFetch(
+      getApiHost() +
+        `/${year}/${semester}/${encodeURIComponent(
+          campus
+        )}/vacant/${building}/classrooms`,
+      {
+        method: 'get',
+      }
+    )
+
+    return err ? null : data
   }
 }
