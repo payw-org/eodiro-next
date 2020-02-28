fa5554016233d1334df476e625aa1242410414fd
Jang Haemin <io@jhaemin.com>
2020-02-29 06:52:40 +0900

    dev: apply eslint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/no-var-requires': [0],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
+    'react/prop-types': [0],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: false,
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false,
+        },
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': [0],
+    '@typescript-eslint/camelcase': [0],
+  },
+}
