70fdc50fedb09462c1106f7ed718afa3872b75c9
Jang Haemin <io@jhaemin.com>
2020-02-27 19:53:25 -0600

    feat(nav): add a mobile support

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navigation.scss'
 import EodiroLogo from '@/components/icons/EodiroLogo'
-import Link from 'next/link'
+import ThreeDotsVerticalColorIcon from '../icons/ThreeDotsVerticalColorIcon'
+import buildClassName from '@/modules/build-class-name'
 
 type NavItemProps = {
   to: string
@@ -10,30 +11,52 @@ type NavItemProps = {
 
 const NavItem: React.FC<NavItemProps> = ({ to, title }) => {
   return (
-    // <Link href={to}>
     <a className="en-menu-link" href={to}>
       <li className="en-menu-item">{title}</li>
     </a>
-    // </Link>
   )
 }
 
 const Navigation: React.FC = () => {
+  const [isMobileNavMenuOpened, setIsMobileNavMenuOpened] = useState(false)
+
   return (
     <nav id="eodiro-navigation">
       <div className="en-wrapper">
-        {/* <Link href="/"> */}
-          <a className="home-link" href='/'>
-            <EodiroLogo className="eodiro-logo" />
-          </a>
-        {/* </Link> */}
-        <ul className="en-menus-container">
+        <a className="home-link" href="/">
+          <EodiroLogo className="eodiro-logo" appearance="dark" fill="#fff" />
+          <EodiroLogo className="eodiro-logo" appearance="light" fill="#000" />
+        </a>
+        <ul
+          className={buildClassName(
+            'en-menus-container',
+            isMobileNavMenuOpened ? 'opened' : ''
+          )}
+        >
           <NavItem title="빈 강의실" to="/vacant" />
           <NavItem title="강의 검색" to="/" />
           <NavItem title="학식 메뉴" to="/" />
           <NavItem title="빼빼로 광장" to="/" />
           <NavItem title="더 보기" to="/" />
         </ul>
+        <div className="more-tappable">
+          <ThreeDotsVerticalColorIcon
+            className="more-icon"
+            appearance="light"
+            fill="#000"
+            onClick={(e) => {
+              setIsMobileNavMenuOpened(!isMobileNavMenuOpened)
+            }}
+          />
+          <ThreeDotsVerticalColorIcon
+            className="more-icon"
+            appearance="dark"
+            fill="#fff"
+            onClick={(e) => {
+              setIsMobileNavMenuOpened(!isMobileNavMenuOpened)
+            }}
+          />
+        </div>
       </div>
     </nav>
   )
