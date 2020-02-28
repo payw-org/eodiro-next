b63c79c43639499a052a713900b363223790df21
Jang Haemin <io@jhaemin.com>
2020-02-29 05:03:53 +0900

    chore: apply more detailed semester division

--- src/modules/get-semester.ts
+++ src/modules/get-semester.ts
@@ -2,17 +2,40 @@ import { Semester } from '@/types'
 import dayjs from 'dayjs'
 
 const getSemester = (date: Date = new Date()): Semester => {
-  const now = dayjs()
+  const now = dayjs(date)
   const month = now.month() + 1
+  const nowDate = now.date()
+  const nextSemesterThresholdDate = 20
+
   let semester: Semester
 
-  if (month >= 1 && month <= 2) {
+  if (
+    (month === 12 && nowDate > nextSemesterThresholdDate) ||
+    month === 1 ||
+    (month === 2 && nowDate <= nextSemesterThresholdDate)
+  ) {
     semester = '겨울'
-  } else if (month >= 3 && month <= 6) {
+  } else if (
+    (month === 2 && nowDate > nextSemesterThresholdDate) ||
+    month === 3 ||
+    month === 4 ||
+    month === 5 ||
+    (month === 6 && nowDate <= nextSemesterThresholdDate)
+  ) {
     semester = '1'
-  } else if (month >= 7 && month <= 8) {
+  } else if (
+    (month === 6 && nowDate > nextSemesterThresholdDate) ||
+    month === 7 ||
+    (month === 8 && nowDate <= nextSemesterThresholdDate)
+  ) {
     semester = '여름'
-  } else if (month >= 9 && month <= 12) {
+  } else if (
+    (month === 8 && nowDate > nextSemesterThresholdDate) ||
+    month === 9 ||
+    month === 10 ||
+    month === 11 ||
+    (month === 12 && nowDate <= nextSemesterThresholdDate)
+  ) {
     semester = '2'
   }
 
