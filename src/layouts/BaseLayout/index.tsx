4e95d13cd6369829ca97a72d956bd92198d9f5d3
Jang Haemin <io@jhaemin.com>
2020-02-29 05:02:36 +0900

    chore: add a top gap option

--- src/layouts/BaseLayout/index.tsx
+++ src/layouts/BaseLayout/index.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import Navigation from '@/components/Navigation'
 import './BaseLayout.scss'
+import buildClassName from '@/modules/build-class-name'
 
-const BaseLayout: React.FC = ({ children }) => {
+type BaseLayoutProps = {
+  appClassName?: string
+  bodyClassName?: string
+  hasTopGap?: boolean
+}
+
+const BaseLayout: React.FC<BaseLayoutProps> = ({
+  children,
+  appClassName,
+  bodyClassName,
+  hasTopGap = false,
+}) => {
   return (
-    <div id="eodiro-app">
+    <div id="eodiro-app" className={appClassName}>
       <Navigation />
-      <div className="body-content">{children}</div>
+      <div
+        className={buildClassName(
+          'body-content',
+          bodyClassName,
+          hasTopGap ? 'top-gap' : ''
+        )}
+      >
+        {children}
+      </div>
     </div>
   )
 }
