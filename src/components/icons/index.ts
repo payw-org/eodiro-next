4574106c99c6d0a2e966c02bc046c2b79be76ee9
Jang Haemin <io@jhaemin.com>
2020-02-29 17:03:50 +0900

    chore: re-export icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,8 @@
+export { default as ArrowRightColorIcon } from './ArrowRightColorIcon'
+export { default as CafeteriaIconColor } from './CafeteriaIconColor'
+export { default as EodiroLogo } from './EodiroLogo'
+export { default as LecturesIconColor } from './LecturesIconColor'
+export { default as OpensourceIconColor } from './OpensourceIconColor'
+export { default as SquareIconColor } from './SquareIconColor'
+export { default as ThreeDotsVerticalColorIcon } from './ThreeDotsVerticalColorIcon'
+export { default as VacantIconColor } from './VacantIconColor'
