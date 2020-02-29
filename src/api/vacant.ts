042f2e14d93ea69017242d139e56c2ddbd8cc175
Jang Haemin <io@jhaemin.com>
2020-02-29 17:03:30 +0900

    feat: vacant

--- src/api/vacant.ts
+++ src/api/vacant.ts
@@ -1,6 +1,26 @@
 import getApiHost from '@/modules/get-api-host'
 import { Campus, Year, Semester } from '@/types'
 import useFetch from '@/modules/use-fetch'
+import dayjs from 'dayjs'
+import getSemester from '@/modules/get-semester'
+
+export type VacantBuildings = {
+  building_number: string
+  total: number
+  empty: number
+}[]
+
+export type VacantClassrooms = {
+  classroom_number: string
+  lectures: {
+    name: string
+    professor: string
+    start_h: number
+    start_m: number
+    end_h: number
+    end_m: number
+  }[]
+}[]
 
 export default class VacantApi {
   static async buildings({
@@ -11,7 +31,11 @@ export default class VacantApi {
     year: Year
     semester: Semester
     campus: Campus
-  }) {
+  }): Promise<VacantBuildings | null> {
+    const now = dayjs()
+    year = year || now.year()
+    semester = semester || getSemester()
+
     const [err, data] = await useFetch(
       getApiHost() +
         `/${year}/${encodeURIComponent(semester)}/${encodeURIComponent(
@@ -31,11 +55,15 @@ export default class VacantApi {
     campus,
     building,
   }: {
-    year: Year
-    semester: Semester
+    year?: Year
+    semester?: Semester
     campus: Campus
     building: string
-  }) {
+  }): Promise<VacantClassrooms | null> {
+    const now = dayjs()
+    year = year || now.year()
+    semester = semester || getSemester()
+
     const [err, data] = await useFetch(
       getApiHost() +
         `/${year}/${encodeURIComponent(semester)}/${encodeURIComponent(
---
+++ src/pages/vacant/[buildingNumber]/index.tsx
@@ -0,0 +1,136 @@
+import React from 'react'
+import dayjs from 'dayjs'
+import { NextPage } from 'next'
+import { VacantClassrooms } from '@/api/vacant'
+import { VacantApi } from '@/api'
+import BaseLayout from '@/layouts/BaseLayout'
+import ArrowBlock from '@/components/ui/ArrowBlock'
+import Grid from '@/layouts/Grid'
+import ServerError from '@/components/ServerError'
+import buildClassName from '@/modules/build-class-name'
+
+import './VacantClassroomsPage.scss'
+
+type VacantClassroomsPageProps = {
+  buildingNumber: string
+  classroomsInfo: VacantClassrooms
+}
+
+const VacantClassroomsPage: NextPage<VacantClassroomsPageProps> = ({
+  buildingNumber,
+  classroomsInfo,
+}) => {
+  const now = dayjs()
+  const nowAccumMin = now.hour() * 60 + now.minute()
+  // const nowAccumMin = 720
+
+  return (
+    <BaseLayout hasTopGap>
+      <div id="eodiro-vacant-classrooms">
+        <h1 className="building-number">🏢 {buildingNumber}관</h1>
+        {classroomsInfo ? (
+          <Grid>
+            {classroomsInfo.map((info) => {
+              let inClass = false
+              let percentage = 0
+              let remainingHour = 0
+              let remainingMin = Infinity
+              let currentLectureName = ''
+
+              for (let i = 0; i < info.lectures.length; i += 1) {
+                const lecture = info.lectures[i]
+                const startAccumMin = lecture.start_h * 60 + lecture.start_m
+                const endAccumMin = lecture.end_h * 60 + lecture.end_m
+
+                if (
+                  startAccumMin <= nowAccumMin &&
+                  endAccumMin >= nowAccumMin
+                ) {
+                  inClass = true
+                  currentLectureName = lecture.name
+                  percentage =
+                    ((nowAccumMin - startAccumMin) /
+                      (endAccumMin - startAccumMin)) *
+                    100
+                } else {
+                  const sub = startAccumMin - nowAccumMin
+                  if (sub >= 0 && sub < remainingMin) {
+                    remainingMin = sub
+                  }
+                }
+              }
+
+              if (remainingMin !== Infinity) {
+                const calcHour = Math.floor(remainingMin / 60)
+                if (calcHour) {
+                  remainingHour = calcHour
+                  remainingMin -= remainingHour * 60
+                }
+              }
+
+              return (
+                <ArrowBlock
+                  key={info.classroom_number}
+                  className={buildClassName(
+                    'classroom-info-container',
+                    inClass ? 'in-class' : 'vacant'
+                  )}
+                >
+                  <div>
+                    <h1 className="classroom-number">
+                      {info.classroom_number}
+                      <span className="unit">호</span>
+                    </h1>
+                    {inClass ? (
+                      <>
+                        <p className="current-lecture-name">
+                          {currentLectureName}
+                        </p>
+                        <div className="gauge-shell">
+                          <div
+                            className="gauge-value"
+                            style={{
+                              width: `${100 - percentage}%`,
+                            }}
+                          />
+                        </div>
+                      </>
+                    ) : (
+                      <p className="remaining">
+                        {remainingMin === Infinity
+                          ? '더 이상 강의 없음'
+                          : `${remainingHour ? `${remainingHour}시간` : ''}${
+                              remainingMin ? ` ${remainingMin}분` : ''
+                            } 동안 비어 있음`}
+                      </p>
+                    )}
+                  </div>
+                </ArrowBlock>
+              )
+            })}
+          </Grid>
+        ) : (
+          <ServerError />
+        )}
+      </div>
+    </BaseLayout>
+  )
+}
+
+VacantClassroomsPage.getInitialProps = async (
+  ctx
+): Promise<VacantClassroomsPageProps> => {
+  const buildingNumber = ctx.query.buildingNumber as string
+
+  const data = await VacantApi.classrooms({
+    campus: '서울',
+    building: buildingNumber,
+  })
+
+  return {
+    buildingNumber,
+    classroomsInfo: data,
+  }
+}
+
+export default VacantClassroomsPage
