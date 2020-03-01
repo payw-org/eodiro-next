bd04a38b85dc340dc9f344fa7e81496c4d46e041
Jang Haemin <io@jhaemin.com>
2020-03-01 18:53:04 +0900

    chore: apply page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import BaseLayout from '@/layouts/BaseLayout'
 import { NextPage } from 'next'
+import Head from 'next/head'
 import Grid from '@/layouts/Grid'
 import { ColorIcon } from '@/types'
 import {
@@ -62,35 +63,48 @@ const HomePage: NextPage = () => {
   }, [])
 
   return (
-    <BaseLayout>
-      <div id="eodiro-home">
-        <h1 className="header">
-          <span className="name">어디로</span> {emojiList[emojiIndex]}
-        </h1>
-        <p className="manifesto">중앙대 학생들만을 위한 특별한 서비스</p>
-        <div className="features">
-          <Grid>
-            <HomeFeatureBox
-              title="빈 강의실"
-              to="/vacant"
-              Icon={VacantIconColor}
-            />
-            <HomeFeatureBox title="강의 검색" to="/" Icon={LecturesIconColor} />
-            <HomeFeatureBox
-              title="학식 메뉴"
-              to="/"
-              Icon={CafeteriaIconColor}
-            />
-            <HomeFeatureBox title="빼빼로 광장" to="/" Icon={SquareIconColor} />
-            <HomeFeatureBox
-              title="오픈 소스"
-              to="/opensource"
-              Icon={OpensourceIconColor}
-            />
-          </Grid>
+    <>
+      <Head>
+        <title>어디로</title>
+      </Head>
+      <BaseLayout>
+        <div id="eodiro-home">
+          <h1 className="header">
+            <span className="name">어디로</span> {emojiList[emojiIndex]}
+          </h1>
+          <p className="manifesto">중앙대 학생들만을 위한 특별한 서비스</p>
+          <div className="features">
+            <Grid>
+              <HomeFeatureBox
+                title="빈 강의실"
+                to="/vacant"
+                Icon={VacantIconColor}
+              />
+              <HomeFeatureBox
+                title="강의 검색"
+                to="/"
+                Icon={LecturesIconColor}
+              />
+              <HomeFeatureBox
+                title="학식 메뉴"
+                to="/cafeteria"
+                Icon={CafeteriaIconColor}
+              />
+              <HomeFeatureBox
+                title="빼빼로 광장"
+                to="/"
+                Icon={SquareIconColor}
+              />
+              <HomeFeatureBox
+                title="오픈 소스"
+                to="/opensource"
+                Icon={OpensourceIconColor}
+              />
+            </Grid>
+          </div>
         </div>
-      </div>
-    </BaseLayout>
+      </BaseLayout>
+    </>
   )
 }
 
