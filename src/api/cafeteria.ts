cb19129d6abefec09f7b114bc4f3dc5b3916f7c7
Jang Haemin <io@jhaemin.com>
2020-03-01 15:14:49 +0900

    feat: forward response code on third array index

---
+++ src/api/cafeteria.ts
@@ -0,0 +1,35 @@
+import getApiHost from '@/modules/get-api-host'
+import { Campus, Year, Semester } from '@/types'
+import useFetch from '@/modules/use-fetch'
+import dayjs from 'dayjs'
+import { Day } from '@payw/cau-cafeteria-menus-scraper-types'
+
+export type CafeteriaMenus = Omit<Day, 'date'>
+
+export default class CafeteriaApi {
+  static async menus({
+    date = dayjs().format('YYYY-MM-DD'),
+    campus = '서울',
+  }: {
+    date?: string
+    campus?: Campus
+  }): Promise<CafeteriaMenus> {
+    const uriSafeCampus = encodeURIComponent(campus)
+    const [err, data, status] = await useFetch<CafeteriaMenus>(
+      getApiHost() + `/cafeteria/${date}/${uriSafeCampus}/menus`
+    )
+
+    let menusData = data
+
+    // No Content
+    if (status === 204) {
+      menusData = {
+        breakfast: [],
+        lunch: [],
+        supper: [],
+      }
+    }
+
+    return err ? null : menusData
+  }
+}
--- src/modules/use-fetch.ts
+++ src/modules/use-fetch.ts
@@ -1,18 +1,23 @@
 import fetch from 'isomorphic-unfetch'
 
 type UseFetchError = any
-type UseFetchData = any
+type UseFetchStatusCode = number
 
-async function useFetch(
+async function useFetch<T = any>(
   input: RequestInfo,
   init?: RequestInit
-): Promise<[UseFetchError, UseFetchData]> {
+): Promise<[UseFetchError, T, UseFetchStatusCode]> {
+  const res = await fetch(input, init)
   try {
-    const res = await fetch(input, init)
-    return [null, res.json()]
+    try {
+      const data = (await res.json()) as T
+      return [null, data, res.status]
+    } catch (error) {
+      return [null, null, res.status]
+    }
   } catch (err) {
     console.error(err)
-    return [err, null]
+    return [err, null, res.status]
   }
 }
 
