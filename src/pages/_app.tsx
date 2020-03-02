5cab60c686a667a368b36854fe0c82904ef26469
Jang Haemin <io@jhaemin.com>
2020-03-02 13:12:17 +0900

    chore: remove get initial props

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,9 @@
 import React from 'react'
-import App, { AppInitialProps } from 'next/app'
+import App from 'next/app'
 import Head from 'next/head'
-import { NextPageContext } from 'next'
-import ApiHost from '@/modules/api-host'
 import '@/assets/styles/global/globalstyle.scss'
 
 export default class EodiroApp extends App {
-  static async getInitialProps(appContext): Promise<AppInitialProps> {
-    const appProps = await App.getInitialProps(appContext)
-    const { ctx }: { ctx: NextPageContext } = appContext
-    const host = ctx?.req?.headers?.host
-    appProps.pageProps.host = host
-
-    return { ...appProps }
-  }
-
-  componentDidMount(): void {
-    // Set api host on client side
-    const { pageProps } = this.props
-    ApiHost.setHost(pageProps.host)
-  }
-
   public render(): JSX.Element {
     const { Component, pageProps } = this.props
 
