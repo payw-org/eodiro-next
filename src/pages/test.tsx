b81566f81443183ac32021e6b07eaca0c2aa09e0
Jang Haemin <io@jhaemin.com>
2020-03-03 07:58:26 +0900

    chore: create test page for ui development

---
+++ src/pages/test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TestPage: React.FC = () => {
+  return (
+    <>
+      <h1>Heading 1 Heading 1 Heading 1 Heading 1 Heading 1</h1>
+      <h2>Heading 2 Heading 2 Heading 2 Heading 2 Heading 2</h2>
+      <h3>Heading 3 Heading 3 Heading 3 Heading 3 Heading 3</h3>
+      <p>Body Body Body Body Body</p>
+    </>
+  )
+}
+
+export default TestPage
