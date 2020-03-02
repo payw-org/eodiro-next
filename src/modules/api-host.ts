27cecaf47662c31da6dd2f532a83b0a759dd61a6
Jang Haemin <io@jhaemin.com>
2020-03-02 13:11:56 +0900

    chore: simplify api host

--- src/modules/api-host.ts
+++ src/modules/api-host.ts
@@ -1,27 +1,10 @@
 export default class ApiHost {
-  private static HOST = null
-
-  public static setHost(host: string): void {
-    if (host.startsWith('eodiro')) {
-      // eodiro.com
-      host = 'https://api2.eodiro.com'
-    } else if (host.startsWith('dev')) {
-      // dev.eodiro.com
-      host = 'https://dev.api2.eodiro.com'
-    } else {
-      // Dev
-      host = 'http://' + host.split(':')[0] + ':4020'
-    }
-
-    this.HOST = host
-  }
-
   public static getHost(): string {
-    const host = this.HOST
-      ? this.HOST
-      : process.env.NODE_ENV === 'development'
-      ? 'http://localhost:4020'
-      : 'https://api2.eodiro.com'
+    const host =
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:4020'
+        : 'https://api2.eodiro.com'
+    console.log(host)
 
     return host
   }
