e17e15d93a07acda0fbfd68ef7400189ef98d54b
Jang Haemin <io@jhaemin.com>
2020-03-03 19:54:51 +0900

    feat: scroll to bottom

--- src/layouts/BaseLayout/index.tsx
+++ src/layouts/BaseLayout/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import mergeClassName from '@/modules/merge-class-name'
-import Navigation from '@/components/Navigation'
 import GlobalFooter from '@/components/GlobalFooter'
-
+import Navigation from '@/components/Navigation'
+import { CEM } from '@/modules/custom-event-manager'
+import mergeClassName from '@/modules/merge-class-name'
+import React, { useEffect, useRef } from 'react'
 import './BaseLayout.scss'
 
 type BaseLayoutProps = {
   appClassName?: string
   bodyClassName?: string
   hasTopGap?: boolean
+  enableScrollEvent?: boolean
 }
 
 const BaseLayout: React.FC<BaseLayoutProps> = ({
@@ -16,12 +17,47 @@ const BaseLayout: React.FC<BaseLayoutProps> = ({
   appClassName,
   bodyClassName,
   hasTopGap = true,
+  enableScrollEvent = false,
 }) => {
+  const bodyContent = useRef<HTMLDivElement>(null)
+
+  /**
+   * When the `enableScrollEvent` props is set true,
+   * BaseLayout detects scroll to bottom and dispatches a global CEM event
+   */
+  useEffect(() => {
+    if (!enableScrollEvent) {
+      return
+    }
+
+    // Dispatch scrollends event on the first load
+    window.addEventListener('load', () => {
+      CEM.dispatchEvent('scrollends')
+    })
+
+    // Dispatch scrollends event
+    window.addEventListener(
+      'scroll',
+      () => {
+        if (
+          bodyContent.current.getBoundingClientRect().bottom <=
+          window.innerHeight + 50
+        ) {
+          CEM.dispatchEvent('scrollends')
+        }
+      },
+      {
+        passive: true,
+      }
+    )
+  }, [])
+
   return (
     <div id="eodiro-app-scaffold" className={appClassName}>
       <div id="eodiro-app">
         <Navigation />
         <div
+          ref={bodyContent}
           className={mergeClassName(
             'body-content',
             bodyClassName,
