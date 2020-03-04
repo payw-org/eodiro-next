794380465eb7c3ea62bcba2d56f2b279c2fbc681
senpin <vkdnjdldnjsw@naver.com>
2020-03-04 10:36:15 +0900

    feat: add 'get inquiry' api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
 export * from './cafeteria'
+export * from './inquiry'
 export * from './lectures'
 export * from './vacant'
---
+++ src/api/inquiry.ts
@@ -0,0 +1,21 @@
+import ApiHost from '@/modules/api-host'
+import useFetch from '@/modules/use-fetch'
+
+export type InquiryData = {
+  id: number
+  email: string
+  title: string
+  body: string
+  answer?: string
+  uploaded_at: string
+}
+
+export class InquiryApi {
+  static async inquirys(): Promise<InquiryData | null> {
+    const [err, data] = await useFetch(ApiHost.getHost() + `/inquiry`, {
+      method: 'get',
+    })
+    console.log(data)
+    return err ? null : data
+  }
+}
