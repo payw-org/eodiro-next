2779b54815cdc7fa2276d334b2f6e9b325dbc07f
Jang Haemin <io@jhaemin.com>
2020-03-04 15:31:57 +0900

    config(eslint): disable mandatory react import in component

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,18 +10,11 @@ module.exports = {
   ],
   plugins: ['react', 'react-hooks'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'react-hooks/rules-of-hooks': 'error',
+    'react/prop-types': [0],
     '@typescript-eslint/no-empty-function': [0],
     '@typescript-eslint/no-var-requires': [0],
-    'space-before-function-paren': [
-      'error',
-      {
-        anonymous: 'never',
-        named: 'never',
-        asyncArrow: 'always',
-      },
-    ],
-    'react/prop-types': [0],
     '@typescript-eslint/member-delimiter-style': [
       'error',
       {
@@ -37,5 +30,13 @@ module.exports = {
     ],
     '@typescript-eslint/no-explicit-any': [0],
     '@typescript-eslint/camelcase': [0],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
   },
 }
