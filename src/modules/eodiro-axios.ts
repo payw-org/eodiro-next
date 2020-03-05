7ae093a6e69466189da1c38038ddbd158c03e5df
Jang Haemin <io@jhaemin.com>
2020-03-05 23:36:57 +0900

    refactor: use async/await

--- src/modules/eodiro-axios.ts
+++ src/modules/eodiro-axios.ts
@@ -5,61 +5,54 @@
  * @license MIT
  */
 
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
+import EodiroHttpCookie from './eodiro-http-cookie'
 
-export default function eodiroAxios(
+const moduleConsoleTag = '[eodiro-axios]'
+
+export default async function eodiroAxios(
   config: AxiosRequestConfig,
   eodiroAxiosConfig?: {
     requireAuth?: boolean
   }
-): Promise<[any, AxiosResponse, number]> {
-  return new Promise((resolve) => {
-    if (eodiroAxiosConfig && typeof eodiroAxiosConfig !== 'object') {
-      console.error('eodiroAxios - Wrong type of parameter eodiroAxiosConfig')
-      resolve([true, null, null])
-      return
+): Promise<[any, any, number]> {
+  if (eodiroAxiosConfig && typeof eodiroAxiosConfig !== 'object') {
+    console.error('eodiroAxios - Wrong type of parameter eodiroAxiosConfig')
+
+    return [true, null, null]
+  }
+
+  if (eodiroAxiosConfig) {
+    const { requireAuth } = eodiroAxiosConfig
+
+    if (requireAuth) {
+      const cookie = await EodiroHttpCookie.get()
+      const { accessToken, refreshToken } = cookie
+
+      if (!accessToken) {
+        return [true, null, 401]
+      } else {
+        config.headers = {
+          accessToken,
+          refreshToken,
+        }
+      }
+    }
+  }
+
+  try {
+    const res = await axios(config)
+    return [null, res.data, res.status]
+  } catch (err) {
+    console.warn(moduleConsoleTag, err)
+
+    // Network error
+    // Perhaps our server is closed
+    if (!err.response) {
+      console.error(`${moduleConsoleTag} network error`)
+      return [err, null, null]
     }
 
-    // if (eodiroAxiosConfig) {
-    //   const { requireAuth, http } = eodiroAxiosConfig
-
-    //   if (requireAuth) {
-    //     const accessToken = Auth.getAccessToken(http)
-    //     const refreshToken = Auth.getRefreshToken(http)
-
-    //     // If there is no access token avilable,
-    //     // terminate api request and resolve with an error
-    //     if (!accessToken) {
-    //       resolve([true, null])
-    //       return
-    //     } else {
-    //       // Append headers data
-    //       config.headers = {
-    //         accesstoken: accessToken,
-    //         refreshtoken: refreshToken,
-    //       }
-    //     }
-    //   }
-    // }
-
-    axios(config)
-      .then((res) => {
-        resolve([null, res.data, res.status])
-      })
-      .catch((err) => {
-        if (err) {
-          console.error(err)
-        }
-
-        // Network error
-        // Perhaps our server is closed
-        if (!err.response) {
-          console.error('[eodiro-axios] network error')
-          resolve([err, null, null])
-          return
-        }
-
-        resolve([err, null, err.response.status])
-      })
-  })
+    return [err, null, err.response.status]
+  }
 }
