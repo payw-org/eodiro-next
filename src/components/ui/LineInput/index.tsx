fe38b9881fdbd61335bd50e8bd830f3bb69e0f1e
Jang Haemin <io@jhaemin.com>
2020-03-05 23:35:43 +0900

    feat: support keyboard events and additional onEnter util

--- src/components/ui/LineInput/index.tsx
+++ src/components/ui/LineInput/index.tsx
@@ -13,35 +13,62 @@ type LineInputProps = {
   type?: 'text' | 'search' | 'email' | 'password'
   placeholder?: string
   onChangeHook?: LineInputOnChangeHook
+  onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void
+  onKeyPress?: (event: React.KeyboardEvent<HTMLInputElement>) => void
+  onKeyUp?: (event: React.KeyboardEvent<HTMLInputElement>) => void
+  onEnter?: () => void
 }
 
-export const LineInput: React.FC<LineInputProps> = ({
-  value,
-  setValue = (): void => {},
-  className,
-  type = 'text',
-  placeholder,
-  onChangeHook = (): void => {},
-}) => {
-  return (
-    <div className={mergeClassName('eodiro-line-input', className)}>
-      <input
-        value={value}
-        type={type === 'search' ? 'text' : type}
-        spellCheck="false"
-        placeholder={placeholder}
-        className={mergeClassName('li-field', type === 'search' && 'search')}
-        onChange={(e): void => {
-          const value = e.target.value
-          setValue(value)
-          onChangeHook(value)
-        }}
-      />
-      {type === 'search' && (
-        <div className="magnifier-icon-wrapper">
-          <Magnifier className="icon" fill={EodiroColors.green1} />
-        </div>
-      )}
-    </div>
-  )
-}
+export const LineInput = React.forwardRef<HTMLInputElement, LineInputProps>(
+  (
+    {
+      value,
+      setValue = (): void => {},
+      className,
+      type = 'text',
+      placeholder,
+      onChangeHook = (): void => {},
+      onKeyDown,
+      onKeyPress,
+      onKeyUp,
+      onEnter,
+    },
+    ref
+  ) => {
+    return (
+      <div className={mergeClassName('eodiro-line-input', className)}>
+        <input
+          ref={ref}
+          value={value}
+          type={type === 'search' ? 'text' : type}
+          spellCheck="false"
+          placeholder={placeholder}
+          className={mergeClassName('li-field', type === 'search' && 'search')}
+          onChange={(e): void => {
+            const value = e.target.value
+            setValue(value)
+            onChangeHook(value)
+          }}
+          onKeyDown={(e): void => {
+            if (onKeyDown) {
+              onKeyDown(e)
+            }
+
+            if (onEnter && e.key === 'Enter') {
+              onEnter()
+            }
+          }}
+          onKeyPress={onKeyPress}
+          onKeyUp={onKeyUp}
+        />
+        {type === 'search' && (
+          <div className="magnifier-icon-wrapper">
+            <Magnifier className="icon" fill={EodiroColors.green1} />
+          </div>
+        )}
+      </div>
+    )
+  }
+)
+
+LineInput.displayName = 'LineInput'
