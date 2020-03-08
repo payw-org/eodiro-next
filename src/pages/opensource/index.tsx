39c5d13712bbfd494b2bff5616b33be51bd32dd7
Jang Haemin <io@jhaemin.com>
2020-03-08 11:48:39 +0900

    fix: pageTitle

--- src/pages/opensource/index.tsx
+++ src/pages/opensource/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { NextPage } from 'next'
 import BaseLayout from '@/layouts/BaseLayout'
+import { NextPage } from 'next'
+import React from 'react'
 
 const OpenSourcePage: NextPage = () => {
   return (
-    <BaseLayout hasTopGap>
+    <BaseLayout hasTopGap pageTitle="오픈소스">
       <div id="eodiro-opensource">
         <h1>Open Source</h1>
       </div>
