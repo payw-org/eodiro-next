6d52ecdd74d46feefe132f92f5a1a492d553a365
Jang Haemin <io@jhaemin.com>
2020-03-19 17:48:05 +0900

    feat: use production API server

--- src/modules/api-host.ts
+++ src/modules/api-host.ts
@@ -1,7 +1,20 @@
+/**
+ * Pass --useProdApi to use stable production API version
+ * published on the server
+ */
+
+if (process.env.npm_config_useprodapi) {
+  console.log('[ApiHost] using production API')
+}
+
 export default class ApiHost {
   public static getHost(): string {
+    if (process.env.npm_config_useprodapi) {
+      return 'https://api2.eodiro.com'
+    }
+
     const host =
-      process.env.API_ENV === 'development'
+      process.env.NODE_ENV === 'development'
         ? 'http://localhost:4020'
         : 'https://api2.eodiro.com'
 
