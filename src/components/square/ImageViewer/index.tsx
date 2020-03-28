525df8fadd89958f8e1d11f330fe7cc261ccea86
Jang Haemin <io@jhaemin.com>
2020-03-28 22:10:50 +0900

    feat: image viewer

--- src/components/square/ImageViewer/index.tsx
+++ src/components/square/ImageViewer/index.tsx
@@ -1,10 +1,65 @@
+import { useEffect, useRef } from 'react'
+import Swiper from 'swiper'
+import 'swiper/swiper.scss'
+import './ImageViewer.scss'
+
 type ImageViewerProps = {
-  src: string
+  srcs: string[]
+  close: () => void // Close function
+  initialIndex: number
 }
 const ImageViewer: React.FC<ImageViewerProps> = (props) => {
+  const swiperElm = useRef<HTMLDivElement>(null)
+  const prevElm = useRef<HTMLButtonElement>(null)
+  const nextElm = useRef<HTMLButtonElement>(null)
+  const swiper = useRef<Swiper>(null)
+
+  useEffect(() => {
+    swiper.current = new Swiper(swiperElm.current, {
+      direction: 'horizontal',
+      loop: false,
+      navigation: {
+        prevEl: prevElm.current,
+        nextEl: nextElm.current,
+      },
+      initialSlide: props.initialIndex,
+    })
+  }, [])
+
+  useEffect(() => {
+    swiper.current.slideTo(props.initialIndex, 0)
+  }, [props.initialIndex])
+
   return (
-    <div className="image-viewer">
-      <img src={props.src} alt="" />
+    <div className="post-image-viewer swiper-container" ref={swiperElm}>
+      <div className="background" />
+      <div className="swiper-wrapper">
+        {props.srcs.map((src, i) => {
+          return (
+            <div key={i} className="image-wrapper swiper-slide">
+              <a
+                href={src}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="img-link"
+              >
+                <img src={src} alt={src} />
+              </a>
+            </div>
+          )
+        })}
+      </div>
+
+      <button className="close-btn" onClick={() => props.close()}>
+        <i className="octicon octicon-x" />
+      </button>
+
+      <button className="nav-btn prev-btn" ref={prevElm}>
+        <i className="octicon octicon-chevron-left" />
+      </button>
+      <button className="nav-btn next-btn" ref={nextElm}>
+        <i className="octicon octicon-chevron-right" />
+      </button>
     </div>
   )
 }
--- src/components/square/PostViewerFileContainer/index.tsx
+++ src/components/square/PostViewerFileContainer/index.tsx
@@ -1,5 +1,7 @@
 import ApiHost from '@/modules/api-host'
 import { GetPostById } from '@payw/eodiro-one-api/api/one/scheme'
+import { useState } from 'react'
+import ImageViewer from '../ImageViewer'
 import './style.scss'
 
 type PostViewerFileContainerProps = {
@@ -9,6 +11,9 @@ type PostViewerFileContainerProps = {
 export const PostViewerFileContainer: React.FC<PostViewerFileContainerProps> = (
   props
 ) => {
+  const [isImageViewerOpen, setIsImageViewerOpen] = useState(false)
+  const [initialImageIndex, setInitialImageIndex] = useState(0)
+
   const imageFiles = props.files.filter((file) =>
     file.mimeType.startsWith('image/')
   )
@@ -21,13 +26,34 @@ export const PostViewerFileContainer: React.FC<PostViewerFileContainerProps> = (
     <div id="post-viewer-file-container">
       {/* Images */}
       <div className="images">
-        {imageFiles.map((image) => {
+        {imageFiles.map((image, i) => {
           return (
-            <div className="image-container" key={image.fileId}>
+            <div
+              className="image-container"
+              key={image.fileId}
+              onClick={() => {
+                setInitialImageIndex(i)
+                setIsImageViewerOpen(true)
+              }}
+            >
               <img src={ApiHost.getHost(true) + image.path} alt={image.name} />
             </div>
           )
         })}
+      </div>
+
+      <div
+        style={{
+          visibility: isImageViewerOpen ? 'visible' : 'hidden',
+        }}
+      >
+        <ImageViewer
+          srcs={imageFiles.map((image) => ApiHost.getHost(true) + image.path)}
+          close={() => {
+            setIsImageViewerOpen(false)
+          }}
+          initialIndex={initialImageIndex}
+        />
       </div>
 
       {/* Other downloadable files */}
