643078e5779f3dff956a52740340f266081bce63
Jang Haemin <io@jhaemin.com>
2020-04-05 22:35:53 +0900

    chore: space

--- src/modules/string-utils.ts
+++ src/modules/string-utils.ts
@@ -1,4 +1,4 @@
-export const camelToKebab = function(str: string) {
+export const camelToKebab = function (str: string) {
   return str
     .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
     .replace(/([A-Z])([A-Z])(?=[a-z])/g, '$1-$2')
