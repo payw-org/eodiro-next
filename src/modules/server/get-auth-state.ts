5e13760093133f543e8bfbbea18426aa2d15b021
Jang Haemin <io@jhaemin.com>
2020-04-07 21:59:22 +0900

    refactor: remove duplicate code

--- src/modules/server/get-auth-state.ts
+++ src/modules/server/get-auth-state.ts
@@ -2,11 +2,13 @@ import { AuthApi, Tokens } from '@/api'
 import { IncomingMessage, ServerResponse } from 'http'
 import ApiHost from '../api-host'
 import eodiroAxios from '../eodiro-axios'
+import { reload } from './reload'
 
-type AuthState = {
+export type AuthState = {
   isSigned: boolean
   isAdmin: boolean
   userId: number
+  accessToken: string
 }
 
 /**
@@ -21,45 +23,14 @@ export async function getAuthState(
 ): Promise<AuthState> {
   const { req, res } = ctx
   const tokens = await Tokens.get(req)
-  const authState: AuthState = {
+  let authState: AuthState = {
     isSigned: false,
     isAdmin: false,
     userId: null,
+    accessToken: null,
   }
 
-  // Verify token
-  if (tokens.accessToken) {
-    const authCheck = await AuthApi.isSigned(req)
-
-    if (authCheck.isSigned) {
-      // Token is signed and available to be used with
-      authState.isSigned = authCheck.isSigned
-      authState.isAdmin = authCheck.isAdmin
-      authState.userId = authCheck.userId
-    } else {
-      // The access token has been expired and
-      // needs to be refresehd with the refresh token
-      const newTokens = await AuthApi.refresh(req)
-
-      if (newTokens) {
-        const [err, authCheck] = await eodiroAxios({
-          method: 'POST',
-          url: ApiHost.getHost() + `/auth/is-signed-in`,
-          headers: {
-            accessToken: newTokens.accessToken,
-          },
-        })
-
-        if (!err) {
-          await Tokens.set(newTokens, { req, res })
-          authState.isSigned = authCheck.isSigned
-          authState.isAdmin = authCheck.isAdmin
-          authState.userId = authCheck.userId
-        }
-      }
-    }
-  } else if (tokens.refreshToken) {
-    // Access token has expired and refresh token is available
+  async function refresh() {
     const newTokens = await AuthApi.refresh(req)
 
     if (newTokens) {
@@ -73,11 +44,29 @@ export async function getAuthState(
 
       if (!err) {
         await Tokens.set(newTokens, { req, res })
-        authState.isSigned = authCheck.isSigned
-        authState.isAdmin = authCheck.isAdmin
-        authState.userId = authCheck.userId
+        authState = authCheck
+        reload(req, res)
       }
     }
+  }
+
+  // Verify token
+  if (tokens.accessToken) {
+    const authCheck = await AuthApi.isSigned(req)
+
+    if (authCheck.isSigned) {
+      // Token is signed and available to be used with
+      authState = authCheck as AuthState
+    } else {
+      // The access token has been expired and
+      // needs to be refresehd with the refresh token
+
+      await refresh()
+    }
+  } else if (tokens.refreshToken) {
+    // Access token has expired and refresh token is available
+
+    await refresh()
   }
 
   return authState
