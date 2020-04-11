dc4c1b45ea1ba0cd6426f612e822750a50262f6f
Jang Haemin <io@jhaemin.com>
2020-04-11 17:04:47 +0900

    Deprecate mergeClassNames

--- src/modules/merge-class-name.ts
+++ src/modules/merge-class-name.ts
@@ -1,3 +1,6 @@
+/**
+ * @deprecated Use `classnames` npm module instead
+ */
 export default function mergeClassNames(...classNames: string[]): string {
   let fullClassName = ''
   classNames.forEach((className) => {
