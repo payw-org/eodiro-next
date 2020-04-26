57258302571c27896e8e31166f0889ee0ba47af5
jhaemin <io@jhaemin.com>
2020-04-26 15:59:19 +0900

    chore: sum eodiro api1 and api2

--- src/pages/opensource.tsx
+++ src/pages/opensource.tsx
@@ -20,6 +20,14 @@ export const getServerSideProps: GetServerSideProps<OpenSourceProps> = async () 
     method: 'get',
     url: 'https://api.github.com/repos/paywteam/eodiro-next/contributors',
   })
+  const [, api1Contributors] = await eodiroAxios<Contributor[]>({
+    method: 'get',
+    url: 'https://api.github.com/repos/paywteam/eodiro-api/contributors',
+  })
+  const [, api2Contributors] = await eodiroAxios<Contributor[]>({
+    method: 'get',
+    url: 'https://api.github.com/repos/paywteam/eodiro-api2/contributors',
+  })
 
   const contributors = eodiroContributors
 
@@ -32,6 +40,30 @@ export const getServerSideProps: GetServerSideProps<OpenSourceProps> = async () 
       contributors.push(nextUser)
     } else {
       contributors[index].contributions += nextUser.contributions
+    }
+  }
+
+  for (const api1User of api1Contributors) {
+    const index = contributors.findIndex(
+      (user) => user.login === api1User.login
+    )
+
+    if (index === -1) {
+      contributors.push(api1User)
+    } else {
+      contributors[index].contributions += api1User.contributions
+    }
+  }
+
+  for (const api2User of api2Contributors) {
+    const index = contributors.findIndex(
+      (user) => user.login === api2User.login
+    )
+
+    if (index === -1) {
+      contributors.push(api2User)
+    } else {
+      contributors[index].contributions += api2User.contributions
     }
   }
 
